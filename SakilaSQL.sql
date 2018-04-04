d86361494e430875cb0d429292f9cef09391dc3b
heidi_laptop_home <hmlanden@gmail.com>
2018-04-03 17:39:34 -0700

    solved through query 6c

--- SakilaSQL.sql
+++ SakilaSQL.sql
@@ -69,3 +69,127 @@ ALTER TABLE actor CHANGE COLUMN middle_name middle_name BLOB;
 ALTER TABLE actor DROP COLUMN middle_name;
 
 # 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT 
+    last_name, COUNT(last_name) AS 'Num Actors with Last Name'
+FROM
+    actor
+GROUP BY last_name;
+
+# 4b. List last names of actors and the number of actors who have that last name, 
+# but only for names that are shared by at least two actors
+SELECT 
+    last_name, COUNT(last_name) AS 'Num Actors'
+FROM
+    actor
+GROUP BY last_name
+HAVING COUNT(last_name) > 1;
+
+# 4c. Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table 
+# as GROUCHO WILLIAMS, the name of Harpo's second cousin's husband's yoga teacher. 
+# Write a query to fix the record.
+UPDATE actor 
+SET 
+    first_name = 'HARPO'
+WHERE
+    first_name = 'GROUCHO'
+        AND last_name = 'WILLIAMS';
+        
+# 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that 
+# GROUCHO was the correct name after all! In a single query, if the first name of the actor 
+# is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO 
+# GROUCHO, as that is exactly what the actor will be with the grievous error. 
+# BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, 
+# HOWEVER! (Hint: update the record using a unique identifier.)
+
+# Since this question doesn't make sense and seems to be asking for what it doesn't actually want,
+# I just changed it back.
+UPDATE actor 
+SET 
+    first_name = 'GROUCHO'
+WHERE
+    first_name = 'HARPO'
+        AND last_name = 'WILLIAMS';
+
+# 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
+SHOW CREATE TABLE address;
+
+# 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
+# Use the tables staff and address:
+SELECT 
+    first_name, last_name, address
+FROM
+    staff s
+        LEFT JOIN
+    address a ON s.address_id = a.address_id;
+    
+# 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. 
+# Use tables staff and payment.
+SELECT 
+    s.staff_id, first_name, last_name, SUM(amount) AS 'Total Purchases'
+FROM
+    staff s
+        INNER JOIN
+    payment p ON s.staff_id = p.staff_id
+    GROUP BY s.staff_id;
+
+# 6c. List each film and the number of actors who are listed for that film. 
+# Use tables film_actor and film. Use inner join.
+SELECT 
+    title, COUNT(DISTINCT actor_id) AS 'Num Actors'
+FROM
+    film f
+        INNER JOIN
+    film_actor a ON f.film_id = a.film_id
+GROUP BY f.film_id;
+
+# 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT 
+    COUNT(film_id) AS 'Num Copies'
+FROM
+    inventory
+WHERE
+    film_id IN (SELECT 
+            film_id
+        FROM
+            film
+        WHERE
+            title = 'Hunchback Impossible');
+
+# 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
+# List the customers alphabetically by last name:
+
+
+# 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended 
+# consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries 
+# to display the titles of movies starting with the letters K and Q whose language is English.
+
+# 7b. Use subqueries to display all actors who appear in the film Alone Trip.
+
+# 7c. You want to run an email marketing campaign in Canada, for which you will need the names and 
+# email addresses of all Canadian customers. Use joins to retrieve this information.
+
+# 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+# Identify all movies categorized as famiy films.
+
+# 7e. Display the most frequently rented movies in descending order.
+
+# 7f. Write a query to display how much business, in dollars, each store brought in.
+
+# 7g. Write a query to display for each store its store ID, city, and country.
+
+# 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following 
+# tables: category, film_category, inventory, payment, and rental.)
+
+# 8a. In your new role as an executive, you would like to have an easy way of viewing the 
+# Top five genres by gross revenue. Use the solution from the problem above to create a view. 
+# If you haven't solved 7h, you can substitute another query to create a view.
+
+# 8b. How would you display the view that you created in 8a?
+
+# 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+
+
+
+
+
+
